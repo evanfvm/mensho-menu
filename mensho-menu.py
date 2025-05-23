b97b33b2472c74ee9cbbc13fba4ddcc14f0b3064
Evan Fvm <evan.fvm@gmail.com>
2025-05-23 17:11:42 -0600

    no footer

--- mensho-menu.py
+++ mensho-menu.py
@@ -1,5 +1,12 @@
 import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
+hide_streamlit_style = """
+
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 with st.container(border=True):
     pdf_viewer("menu.pdf", width=1000, pages_vertical_spacing=10)
