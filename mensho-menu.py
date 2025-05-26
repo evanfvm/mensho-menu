707cc57e08343eb65f7456851d18a3d4ef79ee5c
Evan Fvm <153629588+evanfvm@users.noreply.github.com>
2025-05-25 21:16:58 -0600

    Update mensho-menu.py

--- mensho-menu.py
+++ mensho-menu.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
 
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
 with st.container(border=True):
     pdf_viewer("menu.pdf", width=1000, pages_vertical_spacing=10)
